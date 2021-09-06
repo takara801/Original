f53fa32043e3d6d0eabfef6e8588c2ecec131e9f
Miu Yamamoto <mi.oct09@gmail.com>
2021-09-06 16:50:28 +0900

    Timerの発火完了

--- Original/ResultViewController.swift
+++ Original/ResultViewController.swift
@@ -13,11 +13,11 @@ class ResultViewController: UIViewController {
     @IBOutlet var countLabel: UILabel!
 //    @IBOutlet var treeImageView: UIImageView!
     
-    var count: Int = 0
+    var timeCount: Int = 0
     var timer: Timer = Timer()
-    
-    //AnimationViewの宣言
-    var animationView = AnimationView()
+//
+//    //AnimationViewの宣言
+//    var animationView = AnimationView()
     
   
     
@@ -25,26 +25,16 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //アニメーションの呼び出し
-        addAnimationView()
-    }
-    
-    //タイマーのカウントのメソッド
-    @objc func up() {
-        count = count + 1
-        
-        let hours = count / 3600
-                let minutes = (count % 3600) / 60
-                let seconds = count % 60
-                countLabel.text = String(format: "%02d:%02d:%02d%", hours, minutes, seconds)
-        
-//        changeImage()
-        
+//        //アニメーションの呼び出し
+//        addAnimationView()
     }
 
     //タイマーを開始するメソッド
     @IBAction func start () {
+        
+        print("Timing started!")
         if !timer.isValid {
+            //タイマーが動作していなかったら動かす
             timer = Timer.scheduledTimer(timeInterval: 1,
                                          target: self,
                                          selector: #selector(self.up),
@@ -52,34 +42,50 @@ class ResultViewController: UIViewController {
                                          repeats: true
                                         )
         }
-       
+    }
+    
+    //タイマーのカウントのメソッド
+    @objc func up() {
+        timeCount = timeCount + 1
+
+        let hours = timeCount / 3600
+        let minutes = (timeCount % 3600) / 60
+        let seconds = timeCount % 60
+        countLabel.text = String(format: "%02d:%02d:%02d%", hours, minutes, seconds)
     }
     
     
     //タイマーを停止するメソッド
-    @IBAction func stop() {
-            if timer.isValid {
+    @IBAction func pause() {
+        if timer.isValid {
+                //タイマーが動作していたら停止する
                 timer.invalidate()
-            }
+        }
     }
     
-    //アニメーションの準備
-    func addAnimationView() {
-
-        //アニメーションファイルの指定
-        animationView = AnimationView(name: "tree1") //ここに先ほどダウンロードしたファイル名を記述（拡張子は必要なし）
-
-        //アニメーションの位置指定（画面中央）
-        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-
-        //アニメーションのアスペクト比を指定＆ループで開始
-        animationView.contentMode = .scaleAspectFit
-        animationView.loopMode = .loop
-        animationView.play()
-
-        //ViewControllerに配置
-        view.addSubview(animationView)
+    @IBAction func reset(){
+        timer.invalidate() //timerの停止
+        timeCount = 0 //countの初期化
+        countLabel.text = String(timeCount)
     }
+    
+//    //アニメーションの準備
+//    func addAnimationView() {
+//
+//        //アニメーションファイルの指定
+//        animationView = AnimationView(name: "75406-looped-404-error-animation") //ここに先ほどダウンロードしたファイル名を記述（拡張子は必要なし）
+//
+//        //アニメーションの位置指定（画面中央）
+//        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+//
+//        //アニメーションのアスペクト比を指定＆ループで開始
+//        animationView.contentMode = .scaleAspectFit
+//        animationView.loopMode = .loop
+//        animationView.play()
+//
+//        //ViewControllerに配置
+//        view.addSubview(animationView)
+//    }
     
    
 //    func changeImage(){
