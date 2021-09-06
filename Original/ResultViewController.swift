7d5410bcdc7692cbc0440b244e28293b49db41fc
Miu Yamamoto <mi.oct09@gmail.com>
2021-09-06 15:34:34 +0900

    Lottieの導入DONE

--- Original/ResultViewController.swift
+++ Original/ResultViewController.swift
@@ -6,25 +6,30 @@
 //
 
 import UIKit
+import Lottie //Lottieのインポート
 
 class ResultViewController: UIViewController {
 
     @IBOutlet var countLabel: UILabel!
-    
-    @IBOutlet var treeImageView: UIImageView!
+//    @IBOutlet var treeImageView: UIImageView!
     
     var count: Int = 0
-    
     var timer: Timer = Timer()
+    
+    //AnimationViewの宣言
+    var animationView = AnimationView()
     
   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //アニメーションの呼び出し
+        addAnimationView()
     }
     
-    
+    //タイマーのカウントのメソッド
     @objc func up() {
         count = count + 1
         
@@ -33,10 +38,11 @@ class ResultViewController: UIViewController {
                 let seconds = count % 60
                 countLabel.text = String(format: "%02d:%02d:%02d%", hours, minutes, seconds)
         
-        changeImage()
+//        changeImage()
         
     }
 
+    //タイマーを開始するメソッド
     @IBAction func start () {
         if !timer.isValid {
             timer = Timer.scheduledTimer(timeInterval: 1,
@@ -48,66 +54,54 @@ class ResultViewController: UIViewController {
         }
        
     }
-
     
+    
+    //タイマーを停止するメソッド
     @IBAction func stop() {
             if timer.isValid {
                 timer.invalidate()
             }
     }
     
-   
-    func changeImage(){
-    
-     if count <= 10 {
-        import UIKit
-        import Lottie //①でインポート済
+    //アニメーションの準備
+    func addAnimationView() {
 
-        class ViewController: UIViewController {
+        //アニメーションファイルの指定
+        animationView = AnimationView(name: "tree1") //ここに先ほどダウンロードしたファイル名を記述（拡張子は必要なし）
 
-            //AnimationViewの宣言
-            var animationView = AnimationView()
+        //アニメーションの位置指定（画面中央）
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
 
-            override func viewDidLoad() {
-                super.viewDidLoad()
+        //アニメーションのアスペクト比を指定＆ループで開始
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
 
-                //アニメーションの呼び出し
-                addAnimationView()
-            }
-
-            //アニメーションの準備
-            func addAnimationView() {
-
-                //アニメーションファイルの指定
-                animationView = AnimationView(name: "tree1") //ここに先ほどダウンロードしたファイル名を記述（拡張子は必要なし）
-
-                //アニメーションの位置指定（画面中央）
-                animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-
-                //アニメーションのアスペクト比を指定＆ループで開始
-                animationView.contentMode = .scaleAspectFit
-                animationView.loopMode = .loop
-                animationView.play()
-
-                //ViewControllerに配置
-                view.addSubview(animationView)
-            }
-      }else if count <= 15 && count > 10 {
-        treeImageView.image = UIImage(named: "tree2")
-      }else if count <= 20 && count > 15 {
-        treeImageView.image = UIImage(named: "tree3")
-      }else if count <= 25 && count > 20 {
-        treeImageView.image = UIImage(named: "tree1")
-      }else if count <= 30 && count > 25 {
-        treeImageView.image = UIImage(named: "tree2")
-      }else if count <= 35 && count > 30 {
-        treeImageView.image = UIImage(named: "tree3")
+        //ViewControllerに配置
+        view.addSubview(animationView)
     }
-}
+    
+   
+//    func changeImage(){
+//
+//     if count <= 10 {
+//
+//      }else if count <= 15 && count > 10 {
+//        treeImageView.image = UIImage(named: "tree2")
+//      }else if count <= 20 && count > 15 {
+//        treeImageView.image = UIImage(named: "tree3")
+//      }else if count <= 25 && count > 20 {
+//        treeImageView.image = UIImage(named: "tree1")
+//      }else if count <= 30 && count > 25 {
+//        treeImageView.image = UIImage(named: "tree2")
+//      }else if count <= 35 && count > 30 {
+//        treeImageView.image = UIImage(named: "tree3")
+//    }
+//}
     
     
     
     
 }
 
-}
+
